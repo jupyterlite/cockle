adc6ca6e660348891548e877673f45f9f389368a
Ian Thomas <ianthomas23@gmail.com>
2024-04-20 22:06:45 +0100

    Use baseDir for NodeFileSystem for similarity with JupyterFileSystem

--- src/node_file_system.ts
+++ src/node_file_system.ts
@@ -1,21 +1,31 @@
 import { IFileSystem } from "./file_system"
 import * as fs from "node:fs/promises"
+import { join } from "node:path"
 
 export class NodeFileSystem implements IFileSystem {
   // To access local file system.
   // Intended for use in testing, but could be used in production too.
+  // Paths are with respect to a baseDir for similarity with JupyterFileSystem.
+  // Path joining on Windows?
+
+  constructor(baseDir: string) {
+    // Check needed that this is a valid directory.
+    this._baseDir = baseDir
+  }
 
   async delete(path: string): Promise<void> {
-    await fs.unlink(path)
+    await fs.unlink(join(this._baseDir, path))
   }
 
   async list(path: string): Promise<string[]> {
-    const filenames = await fs.readdir(path)
+    const filenames = await fs.readdir(join(this._baseDir, path))
     return filenames.sort()
   }
 
   async touch(path: string): Promise<void> {
     // Assume new file
-    await fs.writeFile(path, "")
+    await fs.writeFile(join(this._baseDir, path), "")
   }
+
+  private _baseDir: string
 }
--- tests/file_system.test.ts
+++ tests/file_system.test.ts
@@ -6,27 +6,27 @@ import { IFileSystem } from "../src"
 import { JupyterFileSystem } from "../src/jupyter_file_system"
 import { NodeFileSystem } from "../src/node_file_system"
 
-async function setup(name: string): Promise<[IFileSystem, string]> {
+async function setup(name: string): Promise<IFileSystem> {
   if (name == "jupyter") {
     const cm = new ContentsManagerMock()
     await cm.save("file1")
     await cm.save("file2")
     await cm.save("dirA", { type: "directory" })
-    return [new JupyterFileSystem(cm), ""]
+    return new JupyterFileSystem(cm)
   } else {
-    const dir = await fs.mkdtemp(tmpdir())
-    await fs.writeFile(join(dir, "file1"), "")
-    await fs.writeFile(join(dir, "file2"), "")
-    await fs.mkdir(join(dir, "dirA"))
-    return [new NodeFileSystem(), dir]
+    const baseDir = await fs.mkdtemp(tmpdir())
+    await fs.writeFile(join(baseDir, "file1"), "")
+    await fs.writeFile(join(baseDir, "file2"), "")
+    await fs.mkdir(join(baseDir, "dirA"))
+    return new NodeFileSystem(baseDir)
   }
 }
 
 describe("IFileSystem", () => {
   it.each(["jupyter", "node"])
   ('.list %s', async (name) => {
-    const [fs, dir] = await setup(name)
-    const filenames = await fs.list(dir)
+    const fs = await setup(name)
+    const filenames = await fs.list("")
     expect(filenames).toEqual(["dirA", "file1", "file2"])
   })
 })
@@ -34,9 +34,9 @@ describe("IFileSystem", () => {
 describe("IFileSystem", () => {
   it.each(["jupyter", "node"])
   ('.touch %s', async (name) => {
-    const [fs, dir] = await setup(name)
-    await fs.touch(join(dir, "newfile"))
-    const filenames = await fs.list(dir)
+    const fs = await setup(name)
+    await fs.touch("newfile")
+    const filenames = await fs.list("")
     expect(filenames).toEqual(["dirA", "file1", "file2", "newfile"])
   })
 })
@@ -44,9 +44,9 @@ describe("IFileSystem", () => {
 describe("IFileSystem", () => {
   it.each(["jupyter", "node"])
   ('.delete %s', async (name) => {
-    const [fs, dir] = await setup(name)
-    await fs.delete(join(dir, "file1"))
-    const filenames = await fs.list(dir)
+    const fs = await setup(name)
+    await fs.delete("file1")
+    const filenames = await fs.list("")
     expect(filenames).toEqual(["dirA", "file2"])
   })
 })
