f52a955c409d4ce828f5bcb239380ff7fb4ad861
Ian Thomas <ianthomas23@gmail.com>
2024-04-25 16:47:43 +0100

    Basic echo command

--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -3,6 +3,14 @@ import { Context } from "../context"
 
 export class EchoCommand extends Command {
   override async run(context: Context): Promise<number> {
+
+    // Expecting single argument only.
+    if (context.args.length != 1) {
+      // Write error message to stderr
+      return 1
+    }
+
+    await context.stdout.write(context.args[0])
     return 0
   }
 }
---
+++ tests/commands/echo.test.ts
@@ -0,0 +1,24 @@
+import { file_system_setup } from "../file_system_setup"
+
+import { CommandRegistry } from "../../src/command_registry"
+import { Context } from "../../src/context"
+import { ConsoleOutput } from "../../src/io/console_output"
+
+describe("echo command", () => {
+  it.each(["jupyter", "node"])
+  ('should write to console %s', async (name) => {
+    const fs = await file_system_setup(name)
+    const stdout = new ConsoleOutput()
+    const context = new Context(["some_text"], fs, stdout)
+
+    const spy = jest.spyOn(console, 'log').mockImplementation(() => {})
+
+    const cmd = CommandRegistry.instance().create("echo")
+    expect(cmd).not.toBeNull()
+    const exit_code = await cmd!.run(context)
+    expect(exit_code).toBe(0)
+
+    expect(spy).toHaveBeenCalledWith("some_text");
+    spy.mockRestore();
+  })
+})
