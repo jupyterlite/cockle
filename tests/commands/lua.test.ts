8e253696c28fc07adbb9969a6302feb90b754c3c
DerThorsten <derthorstenbeier@gmail.com>
2024-07-23 10:55:28 +0200

    fixed lua command

--- tests/commands/lua.test.ts
+++ tests/commands/lua.test.ts
@@ -3,8 +3,10 @@ import { shell_setup_simple } from "../shell_setup"
 describe("lua command", () => {
   it("should write to stdout", async () => {
     const { shell, output } = await shell_setup_simple()
-    const code = "print('Hello, world!')"
+    const code = "print(1+1+40)"
     await shell._runCommands(`lua -e ${code}`)
     console.log(output.text)
+    expect(output.text).toEqual("42\r\n")
+
   })
 })
