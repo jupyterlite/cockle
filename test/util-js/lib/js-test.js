577026c2d356830f37b90e3c5565c976ef2cc249
Ian Thomas <ianthomas23@gmail.com>
2025-07-11 12:01:29 +0100

    Comprehensive testing of JavaScriptCommand (#215)
    * Combine js commands into single js-test for testing
    
    * Comprehensive testing of javascript command
    
    * Don't import ansi in js-test.ts
    
    * Update visual image snapshot

--- test/util-js/lib/js-test.js
+++ test/util-js/lib/js-test.js
@@ -2,11 +2,57 @@ var Module = (function (exports) {
     'use strict';
 
     const ExitCode = {
-        SUCCESS: 0};
+        SUCCESS: 0,
+        GENERAL_ERROR: 1};
 
     async function run(context) {
-        const { args, name, stdout } = context;
-        stdout.write(name + ': ' + args.join(','));
+        const { args } = context;
+        if (args.includes('environment')) {
+            context.environment.set('TEST_JS_VAR', '123');
+            context.environment.delete('TEST_JS_VAR2');
+        }
+        if (args.includes('name')) {
+            context.stdout.write(context.name + '\n');
+        }
+        if (args.includes('stdout')) {
+            context.stdout.write('Output line 1\n');
+            context.stdout.write('Output line 2\n');
+        }
+        if (args.includes('stderr')) {
+            context.stderr.write('Error message\n');
+        }
+        if (args.includes('color')) {
+            for (let j = 0; j < 16; j++) {
+                let line = '';
+                for (let i = 0; i < 32; i++) {
+                    // r,g,b in range 0 to 255 inclusive.
+                    const r = (i + 1) * 8 - 1;
+                    const g = 128;
+                    const b = (j + 1) * 16 - 1;
+                    context.stdout.write(`\x1b[38;2;${r};${g};${b}m` + // RGB color.
+                        String.fromCharCode(65 + i) +
+                        "\x1b[1;0m");
+                }
+                context.stdout.write(line + '\n');
+            }
+        }
+        if (args.includes('stdin')) {
+            // Read until EOT, echoing back as upper case.
+            const { stdin, stdout } = context;
+            let stop = false;
+            while (!stop) {
+                const chars = await stdin.readAsync(null);
+                if (chars.length === 0 || chars.endsWith('\x04')) {
+                    stop = true;
+                }
+                else {
+                    stdout.write(chars.toUpperCase());
+                }
+            }
+        }
+        if (args.includes('exitCode')) {
+            return ExitCode.GENERAL_ERROR;
+        }
         return ExitCode.SUCCESS;
     }
 
--- test/util-js/src/js-capitalise.ts
+++
@@ -1,8 +0,0 @@
-import type { IJavaScriptContext } from '@jupyterlite/cockle';
-import { ExitCode } from '@jupyterlite/cockle';
-
-export async function run(context: IJavaScriptContext): Promise<number> {
-  const { args, name, stdout } = context;
-  stdout.write(name + ': ' + args.map(arg => arg.toUpperCase()).join(','));
-  return ExitCode.GENERAL_ERROR;
-}
