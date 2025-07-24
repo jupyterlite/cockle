282bee85d28adbe04472441d12fcfeecf83451d3
martinRenou <martin.renou@gmail.com>
2025-07-24 15:53:46 +0200

    Oups

--- test/integration-tests/shell.test.ts
+++ test/integration-tests/shell.test.ts
@@ -92,7 +92,9 @@ test.describe('Shell', () => {
         'env|grep ?',
         // True and false commands
         'true',
-        'false'
+        'env|grep ?',
+        'false',
+        'env|grep ?'
       ]);
       expect(output[1]).toMatch('\r\n?=2\r\n');
       expect(output[3]).toMatch('\r\n?=0\r\n');
@@ -104,8 +106,8 @@ test.describe('Shell', () => {
       expect(output[13]).toMatch('\r\n?=0\r\n');
       expect(output[14]).toMatch(/Error: cd: too many arguments/);
       expect(output[15]).toMatch('\r\n?=1\r\n');
-      expect(output[16]).toMatch('\r\n?=0\r\n');
-      expect(output[17]).toMatch('\r\n?=1\r\n');
+      expect(output[17]).toMatch('\r\n?=0\r\n');
+      expect(output[19]).toMatch('\r\n?=1\r\n');
     });
 
     test('should support unicode', async ({ page }) => {
